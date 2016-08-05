25f9a7c0d801ae5b78f652d30fdf68e750373f55
joezini <josephlukeedwards@gmail.com>
2016-08-05 19:39:12 +0100

    complete coding, with no tests done yet

--- mastermind.rb
+++ mastermind.rb
@@ -3,6 +3,7 @@ require 'sinatra/reloader' if development?
 
 $varieties = 6
 $turns = 12
+COLORS = ["red", "blue", "green", "yellow", "purple", "orange"];
 @@new_game = true
 @@game_over = false
 
@@ -53,6 +54,26 @@ class Mastermind
 		end
 		[black_pegs, white_pegs]
 	end
+end
+
+get '/' do
+	# If new game - prepare game data
+	if @@new_game
+		code = Mastermind.create_random_code
+		@@game = Game.new(code, 1, [])
+		@@new_game = false
+	end
+	# If continuing game and params supplied (representing new guess) - evaluate guess and add line to prev guesses array
+	guess = params["guess"]
+	if Mastermind.valid_input(guess)
+		black_pegs, white_pegs = Mastermind.rate_guess(@@game.code, guess)
+		if black_pegs == 4 || @@game.turn >= $turns
+			@@game_over = true 
+		end
+		@@game.turns << Turn.new(guess, black_pegs, white_pegs)
+	end
+	# erb execute page - loop through all prev guesses and output, then output a clickable line if not game over
+	erb :index, locals: {game: @@game, game_over: @@game_over, colors: COLORS}
 end
 
 # class Game
@@ -122,148 +143,126 @@ end
 # end
 
 # Records and displays current row
-class Row
-	def initialize(guess)
-		@guess = guess
-	end
+# class Row
+# 	def initialize(guess)
+# 		@guess = guess
+# 	end
 
-	def set_pegs(black, white)
-		@black_pegs = black
-		@white_pegs = white
-	end
+# 	def set_pegs(black, white)
+# 		@black_pegs = black
+# 		@white_pegs = white
+# 	end
 
-	def set_peg_string(peg_string)
-		@peg_string = peg_string
-	end
+# 	def set_peg_string(peg_string)
+# 		@peg_string = peg_string
+# 	end
 
-	def to_s
-		if $mode == 'c'
-			@peg_string = ''
-			if @black_pegs > 0
-				(1..@black_pegs).each {@peg_string << 'O'}
-			end
-			if @white_pegs > 0
-				(1..@white_pegs).each {@peg_string << 'o'}
-			end
-			@peg_string << '.' until @peg_string.length == 4
-		end
+# 	def to_s
+# 		if $mode == 'c'
+# 			@peg_string = ''
+# 			if @black_pegs > 0
+# 				(1..@black_pegs).each {@peg_string << 'O'}
+# 			end
+# 			if @white_pegs > 0
+# 				(1..@white_pegs).each {@peg_string << 'o'}
+# 			end
+# 			@peg_string << '.' until @peg_string.length == 4
+# 		end
 
-		"#{@guess[0]} #{@guess[1]} #{@guess[2]} #{@guess[3]}  --  #{@peg_string}"
-	end
-end
+# 		"#{@guess[0]} #{@guess[1]} #{@guess[2]} #{@guess[3]}  --  #{@peg_string}"
+# 	end
+# end
 
-# Knows the solution and rates guesses
-class Master
-	def initialize(random = true)
-		if random
-			@code = create_random_code($varieties)
-		else
-			@code = CodeValidator.new.code
-		end
-	end
+# # Knows the solution and rates guesses
+# class Master
+# 	def initialize(random = true)
+# 		if random
+# 			@code = create_random_code($varieties)
+# 		else
+# 			@code = CodeValidator.new.code
+# 		end
+# 	end
 
-	def create_random_code(varieties)
-		code = []
-		(0..3).each do |i|
-			code[i] = rand(varieties) + 1
-		end
-		code
-	end
+# 	def create_random_code(varieties)
+# 		code = []
+# 		(0..3).each do |i|
+# 			code[i] = rand(varieties) + 1
+# 		end
+# 		code
+# 	end
 
-	def rate_guess(guess)
-		black_pegs = 0
-		white_pegs = 0
-		not_correct_guesses = []
-		unguessed_codes = []
-		guess.each_with_index do |x, i|
-			if x == @code[i]
-				black_pegs += 1 
-			else
-				not_correct_guesses << x
-				unguessed_codes << @code[i]
-			end
-		end
-		unguessed_codes.each do |u|
-			if not_correct_guesses.include?(u)
-				white_pegs += 1
-				not_correct_guesses.delete_at(not_correct_guesses.index(u))
-			end
-		end
-		[black_pegs, white_pegs]
-	end
+# 	def rate_guess(guess)
+# 		black_pegs = 0
+# 		white_pegs = 0
+# 		not_correct_guesses = []
+# 		unguessed_codes = []
+# 		guess.each_with_index do |x, i|
+# 			if x == @code[i]
+# 				black_pegs += 1 
+# 			else
+# 				not_correct_guesses << x
+# 				unguessed_codes << @code[i]
+# 			end
+# 		end
+# 		unguessed_codes.each do |u|
+# 			if not_correct_guesses.include?(u)
+# 				white_pegs += 1
+# 				not_correct_guesses.delete_at(not_correct_guesses.index(u))
+# 			end
+# 		end
+# 		[black_pegs, white_pegs]
+# 	end
 
-	def rate_guess_easy(guess)
-		output = ['.', '.', '.', '.']
-		not_correct_guesses = []
-		unguessed_codes = []
-		guess.each_with_index do |x, i|
-			if x == @code[i]
-				output[i] = 'O'
-			else
-				not_correct_guesses[i] = x
-				unguessed_codes << @code[i]
-			end
-		end
-		unguessed_codes.each do |u|
-			if not_correct_guesses.include?(u)
-				output[not_correct_guesses.index(u)] = 'o'
-				not_correct_guesses[not_correct_guesses.index(u)] = ''
-			end
-		end
-		output.join
-	end
+# 	def rate_guess_easy(guess)
+# 		output = ['.', '.', '.', '.']
+# 		not_correct_guesses = []
+# 		unguessed_codes = []
+# 		guess.each_with_index do |x, i|
+# 			if x == @code[i]
+# 				output[i] = 'O'
+# 			else
+# 				not_correct_guesses[i] = x
+# 				unguessed_codes << @code[i]
+# 			end
+# 		end
+# 		unguessed_codes.each do |u|
+# 			if not_correct_guesses.include?(u)
+# 				output[not_correct_guesses.index(u)] = 'o'
+# 				not_correct_guesses[not_correct_guesses.index(u)] = ''
+# 			end
+# 		end
+# 		output.join
+# 	end
 
-	def show_solution
-		puts "The solution is #{@code}"
-	end
-end
+# 	def show_solution
+# 		puts "The solution is #{@code}"
+# 	end
+# end
 
-class CodeValidator
-	def code
-		input = '    '
-		until valid_input(input)
-			puts "Please enter a code as 4 digits e.g. 4321:"
-			input = gets.chomp
-		end 
-		[input[0].to_i, input[1].to_i, input[2].to_i, input[3].to_i]
-	end
+# class CodeValidator
+# 	def code
+# 		input = '    '
+# 		until valid_input(input)
+# 			puts "Please enter a code as 4 digits e.g. 4321:"
+# 			input = gets.chomp
+# 		end 
+# 		[input[0].to_i, input[1].to_i, input[2].to_i, input[3].to_i]
+# 	end
 
-	def valid_input(input)
-		correct_length = (input.length == 4)
-		valid_numbers = true
-		(0..3).each do |i|
-			begin
-				valid_numbers = false unless (1..$varieties).include?(input[i].to_i)
-			rescue Exception => e
-				puts "Not enough valid numbers"
-				valid_numbers = false
-			end
-		end
+# 	def valid_input(input)
+# 		correct_length = (input.length == 4)
+# 		valid_numbers = true
+# 		(0..3).each do |i|
+# 			begin
+# 				valid_numbers = false unless (1..$varieties).include?(input[i].to_i)
+# 			rescue Exception => e
+# 				puts "Not enough valid numbers"
+# 				valid_numbers = false
+# 			end
+# 		end
 
-		correct_length && valid_numbers
-	end
-end
+# 		correct_length && valid_numbers
+# 	end
+# end
 
-get '/' do
-	# If new game - prepare game data
-	if @@new_game
-		code = Mastermind.create_random_code
-		@@game = Game.new(code, 1, [])
-		@@new_game = false
-	end
-	# If continuing game and params supplied (representing new guess) - evaluate guess and add line to prev guesses array
-	guess = params["guess"]
-	if Mastermind.valid_input(guess)
-		black_pegs, white_pegs = Mastermind.rate_guess(@@game.code, guess)
-		@@game_over = true if black_pegs == 4
-		@@game.turns << Turn.new(guess, black_pegs, white_pegs)
-	end
-	# erb execute page - loop through all prev guesses and output, then output a clickable line if not game over
 
-end
-
-# Think I need to redesign the Game to be a data object:
-# - Solution
-# - Turn number
-# - Array of prev guesses & their marks
-# then the Ruby can check each guess and add new elements to the array etc.
--- views/mastermind-script.js
+++ views/mastermind-script.js
@@ -1,9 +1,10 @@
 function cycleColor(element) {
-	var colors = ["red", "blue", "green", "yellow", "purple", "orange"];
 	var currColor = findMatchingArrayElement(colors, element.className);
 	var nextColor = findNextColor(colors, currColor);
 	element.className = element.className.replace(colors[currColor], colors[nextColor]);
+	updateGuess(element, colors)
 };
+
 function findMatchingArrayElement(colorArray, pegClass) {
 	for (i = 0; i < colorArray.length; i++) {
 		if (pegClass.indexOf(colorArray[i]) >= 0) {
@@ -11,6 +12,7 @@ function findMatchingArrayElement(colorArray, pegClass) {
 		};
 	};
 };
+
 function findNextColor(colorArray, index) {
 	if (index === colorArray.length - 1) {
 		return 0;
@@ -18,3 +20,12 @@ function findNextColor(colorArray, index) {
 		return index + 1;
 	};
 };
+
+function updateGuess(element, colorArray) {
+	var currGuess = document.getElementsByName("guess")[0].value
+	var currPeg = parseInt(element.id)
+	var thisPegValue = parseInt(currGuess[currPeg])
+	var newPegValue = findNextColor(colorArray, thisPegValue)
+	var newGuess = currGuess.substring(0,currPeg-1) + newPegValue.toString() + currGuess.substring(currPeg+1,colorArray.length)
+	document.getElementsByName("guess")[0].value = newGuess
+parseInt("string")
