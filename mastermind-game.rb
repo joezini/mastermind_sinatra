87e81c8b9f4d6c4dd48ffa0454cd9c2b0ae9c812
joezini <josephlukeedwards@gmail.com>
2016-08-06 20:27:13 +0100

    add session support

--- mastermind-game.rb
+++ mastermind-game.rb
@@ -2,31 +2,33 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require_relative 'mastermind'
 
+enable :sessions
+
 $varieties = 6
 $turns = 12
 COLORS = ["red", "blue", "green", "yellow", "purple", "orange"];
-@@game_over = false
 
 Game = Struct.new(:solution, :turn, :guesses)
 Turn = Struct.new(:guess, :black_pegs, :white_pegs)
 
 get '/' do
 	if params["new-game"] == "true"
-		@@game = Game.new(Mastermind.create_random_code, 1, [])
-		@@game_over = false
+		session['game'] = Game.new(Mastermind.create_random_code, 1, [])
+		session['game_over'] = false
 	else
-		@@game ||= Game.new(Mastermind.create_random_code, 1, [])
-	end
-	
-	guess = params["guess"]
-	if Mastermind.valid_input(guess)
-		black_pegs, white_pegs = Mastermind.rate_guess(@@game.solution, guess)
-		if black_pegs == 4 || @@game.turn >= $turns
-			@@game_over = true 
-		end
-		@@game.guesses << Turn.new(guess, black_pegs, white_pegs)
-		@@game.turn += 1
+		session['game'] ||= Game.new(Mastermind.create_random_code, 1, [])
+		session['game_over'] ||= false
 	end
 
-	erb :index, locals: {game: @@game, game_over: @@game_over, colors: COLORS}
+	guess = params["guess"]
+	if Mastermind.valid_input(guess)
+		black_pegs, white_pegs = Mastermind.rate_guess(session[:game].solution, guess)
+		if black_pegs == 4 || session['game'].turn >= $turns
+			session['game_over'] = true 
+		end
+		session[:game].guesses << Turn.new(guess, black_pegs, white_pegs)
+		session[:game].turn += 1
+	end
+
+	erb :index, locals: {game: session[:game], game_over: session[:game_over], colors: COLORS}
 end
